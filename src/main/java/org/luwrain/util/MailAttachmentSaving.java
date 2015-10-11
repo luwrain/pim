89343a776bf5d2932e1bffce43aa52026c996202
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2015-10-11 21:43:32 +0600

    Forgotten file of the previous commit

---
+++ src/main/java/org/luwrain/util/MailAttachmentSaving.java
@@ -0,0 +1,40 @@
+
+package org.luwrain.util;
+
+import java.io.*;
+import java.nio.file.*;
+import org.luwrain.core.NullCheck;
+
+class MailAttachmentSaving extends MimePartCollector
+{
+    static final int SUCCESS = 0;
+    static final int NOT_FOUND = 1;
+
+    private String fileName;
+    private File destFile;
+    private int result = NOT_FOUND;
+
+    MailAttachmentSaving(String fileName, File destFile)
+    {
+	this.fileName = fileName;
+	this.destFile = destFile;
+	NullCheck.notNull(fileName, "fileName");
+	NullCheck.notNull(destFile, "destFile");
+	if (fileName.isEmpty())
+	    throw new IllegalArgumentException("fileName may not be null");
+    }
+
+    @Override protected void onAttachment(String fileName, Object obj) throws IOException
+    {
+	if (fileName == null || !fileName.equals(this.fileName))
+	    return;
+	final FilterInputStream s = (FilterInputStream)obj;
+	Files.copy(s, destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+	result = SUCCESS;
+    }
+
+int result()
+    {
+	return result;
+    }
+}
