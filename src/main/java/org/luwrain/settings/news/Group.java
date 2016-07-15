fd99ee1cbc317d452f5d46213037672c98383808
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-07-15 18:13:49 +0700

    Forgotten Group.java

---
+++ src/main/java/org/luwrain/settings/news/Group.java
@@ -0,0 +1,91 @@
+
+package org.luwrain.settings.news;
+
+import org.luwrain.core.*;
+import org.luwrain.core.events.*;
+import org.luwrain.controls.*;
+import org.luwrain.cpanel.*;
+import org.luwrain.pim.*;
+import org.luwrain.pim.news.*;
+
+class Group extends FormArea implements SectionArea
+{
+    private ControlPanel controlPanel;
+    private Luwrain luwrain;
+    private Strings strings;
+    private NewsStoring storing;
+    private StoredNewsGroup group;
+
+    Group(ControlPanel controlPanel, Strings strings,
+	  NewsStoring storing, StoredNewsGroup group) throws PimException
+    {
+	super(new DefaultControlEnvironment(controlPanel.getCoreInterface()), strings.groupFormName());
+	NullCheck.notNull(controlPanel, "controlPanel");
+	NullCheck.notNull(strings, "strings");
+	NullCheck.notNull(storing, "storing");
+	NullCheck.notNull(group, "group");
+	this.controlPanel = controlPanel;
+	this.strings = strings;
+	this.group = group;
+	this.storing = storing;
+	this .luwrain = controlPanel.getCoreInterface();
+	fillForm();
+    }
+
+    private void fillForm() throws PimException
+    {
+	addEdit("name", strings.groupFormTitle(), group.getName());
+	final String[] urls = group.getUrls();
+	final StringBuilder b = new StringBuilder();
+	for(String s: urls)
+	    b.append(s + "\n");
+	activateMultilineEdit("", new String(b));
+    }
+
+    @Override public boolean saveSectionData()
+    {
+	try {
+	    group.setName(getEnteredText("name"));
+	    group.setUrls(getMultilineEditText().split("\n", -1));
+	    return true;
+	}
+	catch(PimException e)
+	{
+	    luwrain.crash(e);
+	    return true;
+	}
+    }
+
+    @Override public boolean onKeyboardEvent(KeyboardEvent event)
+    {
+	NullCheck.notNull(event, "event");
+	if (controlPanel.onKeyboardEvent(event))
+	    return true;
+	return super.onKeyboardEvent(event);
+    }
+
+    @Override public boolean onEnvironmentEvent(EnvironmentEvent event)
+    {
+	NullCheck.notNull(event, "event");
+	if (controlPanel.onEnvironmentEvent(event))
+	    return true;
+	return super.onEnvironmentEvent(event);
+    }
+
+    static Group create(ControlPanel controlPanel,
+			    NewsStoring storing, long id)
+    {
+	NullCheck.notNull(controlPanel, "controlPanel");
+	NullCheck.notNull(storing, "storing");
+	final Luwrain luwrain = controlPanel.getCoreInterface();
+	final Strings strings = (Strings)luwrain.i18n().getStrings(Strings.NAME);
+	try {
+	    return new Group(controlPanel, strings, storing, storing.loadGroupById(id));
+	}
+	catch(PimException e)
+	{
+	    luwrain.crash(e);
+	    return null;
+	}
+    }
+}
