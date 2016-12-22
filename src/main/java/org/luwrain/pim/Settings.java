430f103dedd49078f043f9d6c97c24b6742f2b3d
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-12-23 05:41:13 +0700

    Adding pim.Settings interface

---
+++ src/main/java/org/luwrain/pim/Settings.java
@@ -0,0 +1,22 @@
+
+package org.luwrain.pim;
+
+import org.luwrain.core.*;
+
+public interface Settings
+{
+    static public final String MAIL_PATH = "/org/luwrain/pim/mail";
+
+    public interface MailFolders
+    {
+	String getFolderPending(String defValue);
+	String getFolderSent(String defValue);
+	String getFolderInbox(String defValue);
+    }
+
+    static public MailFolders createMailFolders(Registry registry)
+    {
+	NullCheck.notNull(registry, "registry");
+	return RegistryProxy.create(registry, MAIL_PATH, MailFolders.class);
+    }
+}
