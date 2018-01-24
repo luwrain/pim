255d1a8a59df190b2d1b29faf5eb91233a450fd4
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-01-24 23:23:53 +0700

    Cleaning the fetching strings interface

--- src/main/java/org/luwrain/pim/fetching/Strings.java
+++ src/main/java/org/luwrain/pim/fetching/Strings.java
@@ -20,39 +20,20 @@ package org.luwrain.pim.fetching;
 
 public interface Strings
 {
-    static public final String NAME = "luwrain.pim.fetching";
-    
-    String actionFetchAll();
-    String actionFetchIncomingMail();
-    String actionFetchMail();
-    String actionFetchNews();
-    String actionFetchOutgoingMail();
-    String appName();
+    static public final String NAME = "luwrain.fetching";
+
     String connectingTo(String hostPort);
     String connectionEstablished(String hostPort);
-    String errorFetchingMessagesFromAccount(String accountTitle, String errormsg);
     String errorLoadingMailAccounts(String errorMsg);
     String errorLoadingMailAccount(String uniRef);
-    String errorSavingFetchedMessage();
-    String fetchedMessageSaved(String num, String total, String folderName);
-    String fetchingCompleted();
     String fetchingMailFromAccount(String accountTitle);
-    String interrupted();
-    String introductionAll();
-    String introductionIncomingMail();
-    String introductionMail();
-    String introductionNews();
-    String introductionOutgoingMail();
     String mailAccountDisabled(String accountName);
     String messagesInQueueForAccount(String accountName, String num);
     String newsFetchingError(String groupName);
     String noAllMessagesToBeFetched();
     String noMailAccountsForFetching();
-    String noNewMailInAccount(String accountTitle);
     String noNewsGroups();
     String noNewsGroupsData();
-    String numberOfNewMessages(String num , String accountTitle);
     String sendingMessage(String num, String total);
     String skippingFetchingFromDisabledAccount(String title);
-    String startingNewsFetching();
 }
