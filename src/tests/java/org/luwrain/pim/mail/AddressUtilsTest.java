c9190c49bac1500a3c17ee2da9d10ca0e9724a3d
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-01-30 05:35:43 +0700

    Adding AddressUtilsTest

---
+++ src/tests/java/org/luwrain/pim/mail/AddressUtilsTest.java
@@ -0,0 +1,33 @@
+
+package org.luwrain.pim.mail;
+
+import java.io.*;
+
+import org.junit.*;
+
+import org.luwrain.core.*;
+
+public class AddressUtilsTest extends Assert
+{
+    @Test public void getPersonal() throws Exception
+    {
+	assertTrue(AddressUtils.getPersonal("").equals(""));
+	assertTrue(AddressUtils.getPersonal("test@test.com").equals(""));
+	assertTrue(AddressUtils.getPersonal("<test@test.com>").equals(""));
+	assertTrue(AddressUtils.getPersonal("TEST <test@test.com>").equals("TEST"));
+	assertTrue(AddressUtils.getPersonal(" TEST USER <test@test.com>").equals("TEST USER"));
+	assertTrue(AddressUtils.getPersonal("TEST USER <>").equals(""));
+	assertTrue(AddressUtils.getPersonal("TEST USER").equals(""));
+    }
+
+    @Test public void getAddress()
+    {
+	assertTrue(AddressUtils.getAddress("").equals(""));
+	assertTrue(AddressUtils.getAddress("test@test.com").equals("test@test.com"));
+	assertTrue(AddressUtils.getAddress("<test@test.com>").equals("test@test.com"));
+	assertTrue(AddressUtils.getAddress("TEST <test@test.com>").equals("test@test.com"));
+	assertTrue(AddressUtils.getAddress(" TEST USER <test@test.com>").equals("test@test.com"));
+	assertTrue(AddressUtils.getAddress("TEST USER <>").equals("TEST USER <>"));
+	assertTrue(AddressUtils.getAddress("TEST USER").equals("TEST USER"));
+    }
+}
