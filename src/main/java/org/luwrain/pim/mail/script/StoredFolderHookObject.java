ac54e19257f0d7cbd05ebdcd78d364b64b07df67
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2019-03-10 20:30:31 +0700

    Updating copyright headers in pim.mail

--- src/main/java/org/luwrain/pim/mail/script/StoredFolderHookObject.java
+++ src/main/java/org/luwrain/pim/mail/script/StoredFolderHookObject.java
@@ -1,7 +1,24 @@
+/*
+   Copyright 2012-2019 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+   Copyright 2015 Roman Volovodov <gr.rPman@gmail.com>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
 
 package org.luwrain.pim.mail.script;
 
 import java.util.*;
+import java.util.function.*;
 
 import org.luwrain.core.*;
 import org.luwrain.script.*;
@@ -51,10 +68,25 @@ public final class StoredFolderHookObject extends EmptyHookObject
 		}
 		return ScriptUtils.createReadOnlyArray(res.toArray(new HookObject[res.size()]));
 	    }
+	case "saveMessage":
+	    return (Predicate)this::saveMessage;
 	    	default:
 	    return super.getMember(name);
 	}
     }
 
-    
+    private boolean saveMessage(Object o)
+    {
+	if (o == null || !(o instanceof MessageHookObject))
+	    return false;
+	try {
+	    storing.getMessages().save(folder, ((MessageHookObject)o).message);
+	    return true;
+	}
+	catch(PimException e)
+	{
+	    Log.error(LOG_COMPONENT, "unable to save the message in the stored mail folder:" + e.getClass().getName() + ":" + e.getMessage());
+	    return false;
+	}
+    }
 }
