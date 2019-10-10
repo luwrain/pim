d03c1408ea71636b7a8ac3137d7f5ce0a23ddba4
Michael Pozhidaev <msp@luwrain.org>
2019-10-10 15:00:31 +0700

    The forgotten file of the previous commit

---
+++ src/main/java/org/luwrain/pim/mail/script/MailingListHookObject.java
@@ -0,0 +1,78 @@
+/*
+   Copyright 2012-2019 Michael Pozhidaev <msp@luwrain.org>
+   Copyright 2015 Roman Volovodov <gr.rPman@gmail.com>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.pim.mail.script;
+
+import java.io.*;
+
+import org.luwrain.core.*;
+import org.luwrain.script.*;
+import org.luwrain.pim.*;
+import org.luwrain.pim.mail.*;
+
+final class MailingListHookObject extends EmptyHookObject
+{
+    static private final String LOG_COMPONENT = MailHookObject.LOG_COMPONENT;
+    static private final String HEADER_ID = "list-id:";
+
+    private final String id;
+    private final String name;
+
+    MailingListHookObject(String[] headers)
+    {
+	NullCheck.notNullItems(headers, "headers");
+	String idValue = null;
+	for(String s: headers)
+	    if (s.toLowerCase().startsWith(HEADER_ID))
+	    {
+		idValue = s.substring(HEADER_ID.length());
+		break;
+	    }
+	if (idValue == null || idValue.trim().isEmpty())
+	{
+	    this.id = "";
+	    this.name = "";
+	    return;
+	}
+	try {
+	    idValue = BinaryMessage.decodeText(idValue);
+	}
+	catch(IOException e)
+	{
+	    Log.warning(LOG_COMPONENT, "unable to decode the list ID name:" + e.getClass().getName() + ":" + e.getMessage());
+	}
+	final String idStr = AddressUtils.getAddress(idValue).trim();
+	if (!idStr.isEmpty())
+	    this.id = idStr; else
+	    this.id = idValue.trim();
+	this.name = AddressUtils.getPersonal(idValue);
+    }
+
+    @Override public Object getMember(String name)
+    {
+	NullCheck.notNull(name, "name");
+	switch(name)
+	{
+	case "id":
+	    return this.id;
+	case "name":
+	    return this.name;
+	default:
+	    return super.getMember(name);
+	}
+    }
+}
