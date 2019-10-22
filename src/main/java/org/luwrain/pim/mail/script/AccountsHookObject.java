756dd91dfb256256cff4f185d87bd306987edf51
Michael Pozhidaev <msp@luwrain.org>
2019-10-22 07:43:25 +0700

    Adding mail.AccountsHookObject

---
+++ src/main/java/org/luwrain/pim/mail/script/AccountsHookObject.java
@@ -0,0 +1,63 @@
+/*
+   Copyright 2012-2019 Michael Pozhidaev <msp@luwrain.org>
+   Copyright 2015 Roman Volovodov <gr.rPman@gmail.com>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.pim.mail.script;
+
+import java.util.*;
+import java.util.function.*;
+
+import org.luwrain.core.*;
+import org.luwrain.script.*;
+import org.luwrain.pim.*;
+import org.luwrain.pim.mail.*;
+
+final class AccountsHookObject extends EmptyHookObject
+{
+    static private final String LOG_COMPONENT = MailHookObject.LOG_COMPONENT;
+
+    private final MailStoring storing;
+
+    AccountsHookObject(MailStoring storing)
+    {
+	NullCheck.notNull(storing, "storing");
+	this.storing = storing;
+    }
+
+    @Override public Object getMember(String name)
+    {
+	NullCheck.notNull(name, "name");
+	switch(name)
+	{
+	case "newAccount":
+	    return (Supplier)this::newAccount;
+	default:
+	    return super.getMember(name);
+	}
+    }
+
+    private Object newAccount()
+    {
+	try {
+	    return new AccountHookObject(storing.getAccounts().save(new MailAccount()));
+	}
+	catch(PimException e)
+	{
+	    Log.error(LOG_COMPONENT, "unable to save newly created mail account:" + e.getClass().getName() + ":" + e.getMessage());
+	    return true;
+	}
+    }
+}
