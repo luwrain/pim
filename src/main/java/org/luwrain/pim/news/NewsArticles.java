4ef61690c53799c6c8b5778fe42ddbb9f80cb38f
Michael Pozhidaev <msp@luwrain.org>
2021-04-05 08:35:49 +0700

    Copyright headers update

--- src/main/java/org/luwrain/pim/news/NewsArticles.java
+++ src/main/java/org/luwrain/pim/news/NewsArticles.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2012-2020 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+   Copyright 2012-2021 Michael Pozhidaev <michael.pozhidaev@gmail.com>
    Copyright 2015 Roman Volovodov <gr.rPman@gmail.com>
 
    This file is part of LUWRAIN.
